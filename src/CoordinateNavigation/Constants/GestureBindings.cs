47ba10f398187242eb4b61a790a32fa218fb4b76
MahammadA <mahammada@risk.az>
2024-11-01 22:31:23 +0400

    add gesture bindings for commands

---
+++ src/CoordinateNavigation/Constants/GestureBindings.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace CoordinateNavigation.Constants
+{
+    public static class GestureBindings
+    {
+        public static readonly KeyGesture ToggleCoordinateTypeGesture = new(Key.Space, ModifierKeys.Control);
+        public static readonly KeyGesture CopyGesture = new(Key.C, ModifierKeys.Control);
+        public static readonly KeyGesture PasteGesture = new(Key.V, ModifierKeys.Control);
+        public static readonly KeyGesture UndoGesture = new(Key.Z, ModifierKeys.Control);
+        public static readonly KeyGesture RedoGesture = new(Key.Y, ModifierKeys.Control);
+    }
+}
---
+++ src/CoordinateNavigation/Helpers/Extensions/RelayGestureBinding.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CoordinateNavigation.Helpers.Extensions
+{
+    public class RelayGestureBinding : KeyBinding
+    {
+        // dependency properties
+        public static readonly DependencyProperty CommandBindingProperty = DependencyProperty.Register(
+            nameof(CommandBinding), typeof(RelayCommand), typeof(RelayGestureBinding),
+            new PropertyMetadata(null, OnCommandBindingChanged));
+
+        public static readonly DependencyProperty GestureBindingProperty = DependencyProperty.Register(
+            nameof(GestureBinding), typeof(KeyGesture), typeof(RelayGestureBinding),
+            new PropertyMetadata(null, OnGestureBindingChanged));
+
+
+        // bindings
+        public RelayCommand CommandBinding
+        {
+            get => (RelayCommand)GetValue(CommandBindingProperty);
+            set => SetValue(CommandBindingProperty, value);
+        }
+
+        public KeyGesture GestureBinding
+        {
+            get => (KeyGesture)GetValue(GestureBindingProperty);
+            set => SetValue(GestureBindingProperty, value);
+        }
+
+        // events
+        private static void OnCommandBindingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var keyBinding = (RelayGestureBinding)d;
+            keyBinding.Command = (RelayCommand)e.NewValue;
+        }
+
+        private static void OnGestureBindingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var keyBinding = (RelayGestureBinding)d;
+            if (e.NewValue is KeyGesture keyGesture)
+            {
+                keyBinding.Key = keyGesture.Key;
+                keyBinding.Modifiers = keyGesture.Modifiers;
+            }
+        }
+    }
+}
