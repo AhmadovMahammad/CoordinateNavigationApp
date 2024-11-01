b5d6c6fc10522cf36365f571c1e2ebf162a57caa
MahammadA <mahammada@risk.az>
2024-11-01 21:56:15 +0400

    Add KeyBinding for shortcuts

---
+++ src/CoordinateNavigation/Helpers/Extensions/RelayKeyBinding.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CoordinateNavigation.Helpers.Extensions
+{
+    public class RelayKeyBinding : KeyBinding
+    {
+        public static readonly DependencyProperty CommandBindingProperty = DependencyProperty.Register(
+            nameof(CommandBinding), typeof(RelayCommand), typeof(RelayKeyBinding),
+            new FrameworkPropertyMetadata(OnCommandBindingChanged));
+
+        public RelayCommand CommandBinding
+        {
+            get => (RelayCommand)GetValue(CommandBindingProperty);
+            set => SetValue(CommandBindingProperty, value);
+        }
+
+        private static void OnCommandBindingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var keyBinding = (RelayKeyBinding)d;
+            keyBinding.Command = (RelayCommand)e.NewValue;
+        }
+    }
+}
